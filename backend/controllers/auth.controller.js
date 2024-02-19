677fb8c47b63954855a2788bd45cc793d6f26ae8
Akash <akshalder11@gmail.com>
2024-02-19 13:49:23 +0530

    Build backend - Signup built

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,22 +1,57 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import generateToken from "../utils/generateToken.js"
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
     const { fullname, username, password, confirmPassword, gender } = req.body;
 
-    if(password !== confirmPassword){
-      return res.status(400).json({error: "Passwords don't match"})
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
     }
 
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
 
-    if(user) {
-      return res.status(400).json({error: "User already exists"})
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    //HashPass
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt);
+    //Image
+    const profilePicBoy = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const profilePicGirl = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await User({
+      fullname,
+      username,
+      password: hashPass,
+      gender,
+      profilePic: gender === "male" ? profilePicBoy : profilePicGirl,
+    });
+
+    if (newUser) {
+
+      //JWT
+      generateToken(newUser._id, res);
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
     }
 
 
   } catch {
-    console.log("signUser");
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         enum: ["male", "female"]
     },
-    rofilePic: {
+    profilePic: {
         type: String,
         default: ""
     },
