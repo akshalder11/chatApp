eedd282c779dcd80942df152916c24619bb54794
Akash <akshalder11@gmail.com>
2024-11-10 14:24:22 +0530

    Implementing React Router and ReactForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
-import Login from './pages/Authentication/Login';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Authentication/Login";
+import Signup from "./pages/Authentication/Signup";
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <div className="h-screen flex items-center justify-center mainBack ">
-      <Login/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/logIn' element={<Login/>} />
+        <Route path='/signUp' element={<Signup />} />
+      </Routes>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
