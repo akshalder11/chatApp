dd6313bb6b89716f59113bdff038430baefeb686
Akash <akshalder11@gmail.com>
2024-11-10 15:24:24 +0530

    AuthContext Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Authentication/Login";
 import Signup from "./pages/Authentication/Signup";
 import Home from "./pages/Home/Home";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="h-screen flex items-center justify-center mainBack ">
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/logIn' element={<Login/>} />
-        <Route path='/signUp' element={<Signup />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/logIn"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signUp"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
       </Routes>
-      
     </div>
   );
 }
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(
+    JSON.parse(localStorage.getItem("currentUser")) || null
+  );
+  return (
+    <AuthContext.Provider
+      value={{
+        authUser,
+        setAuthUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
